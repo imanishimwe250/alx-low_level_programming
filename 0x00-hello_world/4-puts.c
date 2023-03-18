8a318ff22091a5916618614b5acc4b50c93929e1
imanishimwe250 <imanishimwehubert691@gmail.com>
2023-03-18 04:39:01 -0700

    5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,9 @@
 	
 
 /**
-* main - this is the main function
-*
-*Return: always 0
+* main - A program that prints a line with puts function 
+*Return: 0 (Success)
 */
-
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
