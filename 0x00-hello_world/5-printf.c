139fd058a73e8477a7a60671442d9513b72618fc
imanishimwe250 <imanishimwehubert691@gmail.com>
2023-03-18 04:46:19 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,13 +3,10 @@
 
 /**
  * main - this is main function
- *
  * Return: 0 when successful
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 	}
-
-
