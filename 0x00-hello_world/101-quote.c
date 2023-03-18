8a2d9b7776b77f1a25d829737712f31b4072195d
imanishimwe250 <imanishimwehubert691@gmail.com>
2023-03-18 15:18:50 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - prints"and that piece of art is useful"-Dora Korpar,2015-10-19"
+ * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19"
+ *
  * Return : always 1
  */
+
 int main(void)
 {
 write(2, 
 	
-	"and that piece of art is useful\"-Dora Korpar,2015-10-19\n",59);
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return(1);
 }
 
